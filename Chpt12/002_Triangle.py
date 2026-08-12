74e6bb95ddb99b5952d68a03e31296f877d3775f
sn0250 <satoshi0520study@gmail.com>
2026-08-12 11:59:53 +0900

    条件チェック

--- Chpt12/002_Triangle.py
+++ Chpt12/002_Triangle.py
@@ -1,8 +1,24 @@
 from math import sqrt
 
+def is_valid(a, b, c):
+    # 数でない
+    if not all(isinstance(x, (int, float)) for x in (a, b, c)):
+        return False
+    # 正でない
+    if not all(x > 0 for x in (a, b, c)):
+        return False
+    # 辺の長さが三角形の成立条件を満たさない
+    # a < b + c ⇔ 2a < a + b + c
+    if not(2 * max(a, b, c) < (a + b + c)):
+        return False
+    return True
+
 class Triangle:
     def __init__(self, a, b, c):
         print("Created!")
+        if (is_valid(a, b, c) == False):
+            print("Invalid value. Set to (0, 0, 0)")
+            a = b = c = 0
         self.a = a
         self.b = b
         self.c = c
@@ -15,6 +31,22 @@ class Triangle:
         S = sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
         print(f"Area = {S}")
 
+# ケース1:正常
 t1 = Triangle(3,4,5)
 t1.show_info()
 t1.area()
+
+# ケース2:文字
+t2 = Triangle('a', 4, 5)
+t2.show_info()
+t2.area()
+
+# ケース3:負数
+t3 = Triangle(-1, 4, 5)
+t3.show_info()
+t3.area()
+
+# ケース4:成立条件不成立
+t4 = Triangle(9999, 4, 5)
+t4.show_info()
+t4.area()
