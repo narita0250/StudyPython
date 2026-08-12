c70a90dea6110dabe63660cd523fd3a5fff4cb03
sn0250 <satoshi0520study@gmail.com>
2026-08-12 11:30:34 +0900

    [Add]インスタンス生成時に型チェック

--- Chpt12/001_Object.py
+++ Chpt12/001_Object.py
@@ -1,5 +1,10 @@
 class Orange:
     def __init__(self, w, c):
+        # 型チェック
+        if not isinstance(w, (int, float)):
+            print("Invalid weight. Set to (0, -)")
+            w = 0
+            c = '-'
         self.weight = w
         self.color = c
         print("Created!")
@@ -9,3 +14,6 @@ class Orange:
 
 or1 = Orange(10, "dark")
 or1.show_info()
+
+or2 = Orange("20", "Yellow")
+or2.show_info()
