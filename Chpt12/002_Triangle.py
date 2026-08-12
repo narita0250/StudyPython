32b2d4cf2f9582b0d1e7f26359cfd33005978ed8
sn0250 <satoshi0520study@gmail.com>
2026-08-12 11:41:45 +0900

    面積を返すareaメソッド

--- Chpt12/002_Triangle.py
+++ Chpt12/002_Triangle.py
@@ -1,5 +1,20 @@
-class Triangle:
-    def __init__(self):
-        print("Created!")
+from math import sqrt
 
-t1 = Triangle()
+class Triangle:
+    def __init__(self, a, b, c):
+        print("Created!")
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def show_info(self):
+        print(f"a = {self.a}, b = {self.b}, c = {self.c}")
+
+    def area(self):
+        s = (self.a + self.b + self.c) / 2
+        S = sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+        print(f"Area = {S}")
+
+t1 = Triangle(3,4,5)
+t1.show_info()
+t1.area()
